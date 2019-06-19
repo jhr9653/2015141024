c2de836ad186f6008140676bbad3c7734b611a45
haram <jhr9653@naver.com>
2019-06-20 01:30:22 +0900

    DB 생성자 필드 선언초기단계

--- file/mongoDB_Test/src/main/java/com/jin/springboot/MyDataMongo.java
+++ file/mongoDB_Test/src/main/java/com/jin/springboot/MyDataMongo.java
@@ -1,5 +1,29 @@
 package com.jin.springboot;
 
-public class MyDataMongo {
+import java.util.Date;
 
-}
+import org.springframework.data.annotation.Id;
+
+public class MyDataMongo {
+	@Id
+
+	private String id;
+	private String name;
+	private int phone;
+	private Date date;
+
+	private String email;
+	private String group;
+	private String memo;
+
+	public MyDataMongo(String name, String id, int phone, String email, String group, String memo) {
+
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.date = new Date();
+		this.id = id;
+		this.email = email;
+		this.group = group;
+		this.memo = memo;
+	}
